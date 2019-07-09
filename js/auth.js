10321ee94298d86d9f64a7ef2f5cb2d0ddcc2d12
smartcloudi <pierrick35+smartcloudi@gmail.com>
2019-07-09 16:24:43 +0200

    handle logout User menu

--- js/auth.js
+++ js/auth.js
@@ -3,7 +3,8 @@ var currentSession = undefined;
 var accessToken = undefined;
 
 const categoriesApiBasePath = "https://g8335hfcy0.execute-api.eu-central-1.amazonaws.com/dev";
-const locationsApiBasePath = "https://6mo2qzbcag.execute-api.eu-central-1.amazonaws.com/dev"
+const locationsApiBasePath = "https://6mo2qzbcag.execute-api.eu-central-1.amazonaws.com/dev";
+const providersApiBasePath = "https://sclx4kb3lk.execute-api.eu-central-1.amazonaws.com/dev";
 
 function isLoggedIn() {
     var data = {
@@ -23,15 +24,27 @@ function isLoggedIn() {
             console.log("Session:", session);
             currentSession = session;
             accessToken = session.idToken.jwtToken;
-            $("#signIn").text("Sign Out");
-            $("#signIn").addClass("btn-outline-info");
-            $("#signIn").removeClass("btn-info");
-            $("#signIn").on("click", () => {
-                if (cognitoUser != null) {
-                    cognitoUser.signOut();
-                    $(location).attr("href", "login.html");
-                }
-            });
+
+            var parsedToken = parseJwt(accessToken);
+            console.log("Parsed Token:", parsedToken);
+            $("#signin").empty();
+            $("#signin").append(`       
+            <nav class="site-navigation float-left">                             
+                <div class="container">
+                    <ul class="site-menu js-clone-nav d-none d-lg-block">
+                        <li class="has-children">
+                            <a href="#">${parsedToken.email}</a>
+                            <ul class="dropdown">
+                                <li><a href="user-profile.html">My Account</a></li>
+                                <li><a href="requests.html">My Requests</a></li>
+                                <li><a href="#" onclick="logout()">Logout</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            `);
+
             $("#businesses").hide();
         });
     } else {
@@ -39,6 +52,26 @@ function isLoggedIn() {
             $(location).attr("href", "login.html");
         }
     }
+}
+
+function logout() {
+    if (cognitoUser != null) {
+        cognitoUser.signOut();
+        $(location).attr("href", "login.html");
+    }
+}
+
+function parseJwt(token) {
+    var base64Url = token.split(".")[1];
+    var base64 = decodeURIComponent(
+        atob(base64Url)
+            .split("")
+            .map(function (c) {
+                return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+            })
+            .join("")
+    );
+    return JSON.parse(base64);
 }
 
 isLoggedIn();
